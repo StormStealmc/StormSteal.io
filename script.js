35f662e83e275bbdcbe635b3060c9d2cb0451bcd
StormStealmc <umershuja567@gmail.com>
2026-07-22 14:02:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function copyIP(){
+    navigator.clipboard.writeText("stormsteal.aternos.me")
+    .then(() => {
+        alert("Java IP Copied!");
+    })
+    .catch(() => {
+        alert("Copy failed. Copy manually: stormsteal.aternos.me");
+    });
+}
 
+
+function copyBedrock(){
+    navigator.clipboard.writeText("22956")
+    .then(() => {
+        alert("Bedrock Port Copied!");
+    })
+    .catch(() => {
+        alert("Copy failed. Copy manually: 22956");
+    });
+}
